bd004f7d8397953ca63439c79be4298182b4fab3
MatheusFC2 <matheusfreitascampos@gmail.com>
2024-04-23 11:17:49 -0300

    feat: set types and class Dto signin and signup

--- api/src/modules/auth/dto/signin.dto.ts
+++ api/src/modules/auth/dto/signin.dto.ts
@@ -1,13 +1,11 @@
 import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
-export class CreateUserDto {
+export class SigninDto {
   @IsString()
   @IsNotEmpty()
-  name: string;
-  @IsNotEmpty()
-  @IsString()
   @IsEmail()
   email: string;
+
   @IsString()
   @IsNotEmpty()
   @MinLength(8)
