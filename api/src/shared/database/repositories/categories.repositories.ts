54dbdf05c809a994678fbd5e3103a08a87131ad1
MatheusFC2 <matheusfreitascampos@gmail.com>
2024-05-07 20:23:24 -0300

    feat: crud categories

--- api/src/shared/database/repositories/categories.repositories.ts
+++ api/src/shared/database/repositories/categories.repositories.ts
@@ -5,7 +5,11 @@ import { type Prisma } from '@prisma/client';
 @Injectable()
 export class CategoriesRepository {
   constructor(private readonly prismaService: PrismaService) {}
+
   findMany(findManyDto: Prisma.CategoryFindManyArgs) {
     return this.prismaService.category.findMany(findManyDto);
+  }
+  findFirst(findFirstDto: Prisma.CategoryFindFirstArgs) {
+    return this.prismaService.category.findFirst(findFirstDto);
   }
 }
