aeb8f58bf04a3404de337a02fe807243db45f193
MatheusFC2 <matheusfreitascampos@gmail.com>
2024-05-12 19:10:28 -0300

    chore: create route structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
-import "./App.css";
+import { Router } from "./Router";
 
 export function App() {
-  return <Routes />;
+  return <Router />;
 }
---
+++ frontend/src/Router/index.tsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AuthGuard } from "./AuthGuard";
+import Login from "../view/pages/Login";
+import Register from "../view/pages/Dashboard";
+import Dashboard from "../view/pages/Register";
+import AuthLayout from "../view/layouts/AuthLayout";
+
+export function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<AuthGuard isPrivate={false} />}>
+          <Route element={<AuthLayout />}>
+            <Route path="/login" element={<Login />} />
+          </Route>
+          <Route path="/register" element={<Register />} />
+        </Route>
+        <Route element={<AuthGuard isPrivate />}>
+          <Route path="/" element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
