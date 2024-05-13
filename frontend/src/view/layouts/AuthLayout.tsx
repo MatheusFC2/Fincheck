ae69ed5f349f190fde6bfc6d23d63b1c57ba2f84
MatheusFC2 <matheusfreitascampos@gmail.com>
2024-05-13 20:14:32 -0300

    fix: styles

--- frontend/src/view/layouts/AuthLayout.tsx
+++ frontend/src/view/layouts/AuthLayout.tsx
@@ -5,35 +5,29 @@ import { Logo } from "../components/Logo";
 export default function AuthLayout() {
   return (
     <div className="flex w-full h-full">
-      {/* Left Side */}
-      <div className="w-full h-full flex pt-60 justify-center items-center flex-col gap-16 lg:w-1/2">
-        <Logo className="h-6 text-gray-500" />
+      <div className="pt-60 w-full h-full sm:w-1/2 flex items-center justify-center flex-col gap-16">
+        <Logo className="h-6 text-gray-500 justify-center items-center" />
 
         <div className="w-full max-w-[504px] px-8">
           <Outlet />
         </div>
       </div>
-      {/* End Left Side */}
 
-      {/* Right Side */}
-      <div className="w-1/2 h-full justify-center items-center p-8 relative hidden lg:flex">
-        {/* Background Image */}
+      <div className="w-full h-full hidden justify-center items-center p-8 sm:flex sm:w-1/2">
         <img
           src={illustration}
+          alt="default illustration"
           className="object-cover w-full h-full max-w-[656px] max-h-[960px] select-none rounded-[32px]"
         />
 
-        {/* Bottom Div Image */}
-        <div className=" max-w-[656px] bottom-8 p-10 bg-white absolute rounded-b-[32px]">
+        <div className="absolute max-w-[656px] bg-white p-10 rounded-b-[32px] bottom-8 mx-8">
           <Logo className="text-teal-900 h-8" />
           <p className="text-gray-700 font-medium text-xl mt-6">
             Gerencie suas finanças pessoais de uma forma simples com o fincheck,
             e o melhor, totalmente de graça!
           </p>
         </div>
-        {/* End Bottom Div Image */}
       </div>
-      {/* End Right Side */}
     </div>
   );
 }
