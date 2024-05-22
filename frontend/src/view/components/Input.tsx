5dc4379b0f171f3a0b4a9ca25a64808f3e3dd9a9
MatheusFC2 <matheusfreitascampos@gmail.com>
2024-05-22 18:03:24 -0300

    feat: input components

--- frontend/src/view/components/Input.tsx
+++ frontend/src/view/components/Input.tsx
@@ -1,20 +1,44 @@
-import { ComponentProps } from "react";
+import { CrossCircledIcon } from "@radix-ui/react-icons";
+import { ComponentProps, forwardRef } from "react";
+import cn from "../../app/utils/cn";
 
-interface InputProps extends ComponentProps<"input"> {}
-
-export function Input({ placeholder, name, id, ...props }: InputProps) {
-  const inputId = id ?? name;
-  return (
-    <div className="relative">
-      <input
-        {...props}
-        name={name}
-        id={inputId}
-        className="bg-white w-full rounded-lg border border-gray-500 px-3 h-[52px] text-gray-800"
-      />
-      <label htmlFor={inputId} className="absolute left-[13px] top-3.5">
-        {placeholder}
-      </label>
-    </div>
-  );
+interface InputProps extends ComponentProps<"input"> {
+  name: string;
+  error?: string;
 }
+
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ placeholder, name, id, className, error, ...props }, ref) => {
+    const inputId = id ?? name;
+    return (
+      <div className="relative">
+        <input
+          {...props}
+          ref={ref}
+          name={name}
+          id={inputId}
+          className={cn(
+            "bg-white rounded-lg w-full border border-gray-500 px-3 h-[52px]  text-gray-800  pt-4  placeholder-shown:pt-0 focus:border-gray-800 transition-all outline-none peer",
+            error && "!border-red-900",
+            className
+          )}
+          placeholder=" "
+        />
+        <label
+          htmlFor={inputId}
+          className="absolute text-xs left-[13px] top-2 pointer-events-none text-gray-700 peer-placeholder-shown:text-base peer-placeholder-shown:top-3.5 transition-all"
+        >
+          {placeholder}
+        </label>
+        {error && (
+          <div className="flex gap-2 items-center mt-2 text-red-900">
+            <CrossCircledIcon />
+            <span className="text-xs">{error}</span>
+          </div>
+        )}
+      </div>
+    );
+  }
+);
+
+Input.displayName = "Input";
