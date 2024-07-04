961ed34a99ce9eaf6bf40490319020343c54dc34
MatheusFC2 <matheusfreitascampos@gmail.com>
2024-07-03 22:48:26 -0300

    feat: expired user session

--- frontend/src/app/contexts/AuthContext.tsx
+++ frontend/src/app/contexts/AuthContext.tsx
@@ -1,5 +1,8 @@
-import { createContext, useCallback, useState } from "react";
+import { createContext, useCallback, useEffect, useState } from "react";
 import { localStorageKeys } from "../config/localStorageKeys";
+import { useQuery } from "@tanstack/react-query";
+import { usersService } from "../services/usersService";
+import toast from "react-hot-toast";
 
 interface AuthContextValue {
   signedIn: boolean;
@@ -27,6 +30,20 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
     localStorage.removeItem(localStorageKeys.ACCESS_TOKEN);
     setSignedIn(false);
   }, []);
+
+  const { isError } = useQuery({
+    queryKey: ["users", "me"],
+    queryFn: () => usersService.me(),
+    enabled: signedIn,
+    staleTime: Infinity,
+  });
+
+  useEffect(() => {
+    if (isError) {
+      toast.error("Sua sessão expirou!");
+      signout();
+    }
+  }, [isError, signout]);
 
   return (
     <AuthContext.Provider value={{ signedIn, signin, signout }}>
